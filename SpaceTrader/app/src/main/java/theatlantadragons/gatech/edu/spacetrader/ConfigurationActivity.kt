7457bcdb2a5287750ca19567eec5cab9e3cef71d
Devarsi Rawal <devarsirawal@gmail.com>
2019-02-16 14:03:56 -0500

    Initial commit

---
+++ SpaceTrader/app/src/main/java/theatlantadragons/gatech/edu/spacetrader/ConfigurationActivity.kt
@@ -0,0 +1,12 @@
+package theatlantadragons.gatech.edu.spacetrader
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfigurationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuration)
+    }
+}
